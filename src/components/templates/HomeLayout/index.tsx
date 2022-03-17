10a1923960a9b4f041c10c9554bc90c7ac28ac87
Do Huu Huy <dohuuhuy2@gmail.com>
2022-03-17 10:49:45 +0700

    v

--- src/components/templates/HomeLayout/index.tsx
+++ src/components/templates/HomeLayout/index.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../Organisms/Footer";
 import Header from "../../Organisms/Header";
+
 import styles from "./styles.module.scss";
 
 const HomeLayout = () => {
